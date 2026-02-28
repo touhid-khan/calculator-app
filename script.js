aaa237aa1d74a02ffc6a7de8b8591243d5a7c3a2
Touhid Khan <touhidkhan86844@gmail.com>
2026-03-01 04:49:58 +0600

    Add dark/light theme toggle feature

--- script.js
+++ script.js
@@ -10,14 +10,27 @@ buttons.forEach(button => {
         if (value === "C") {
             currentInput = "";
             updateDisplay();
-        } else if (value === "=") {
-            calculateResult();
-        } 
-        else if (value === "⌫") {
+
+        } else if (value === "⌫") {
             currentInput = currentInput.slice(0, -1);
             updateDisplay();
-        }
-        else {
+
+        } else if (value === "%") {
+            currentInput = (parseFloat(currentInput) / 100).toString();
+            updateDisplay();
+
+        } else if (value === "x²") {
+            currentInput = (parseFloat(currentInput) ** 2).toString();
+            updateDisplay();
+
+        } else if (value === "√") {
+            currentInput = Math.sqrt(parseFloat(currentInput)).toString();
+            updateDisplay();
+
+        } else if (value === "=") {
+            calculateResult();
+
+        } else {
             currentInput += value;
             updateDisplay();
         }
@@ -38,3 +51,10 @@ function calculateResult() {
         currentInput = "";
     }
 }
+
+
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+});
